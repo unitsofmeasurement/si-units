e56bd39d81312b80097a189fd68de7c8b9abdbe6
Werner Keil <werner.keil@gmx.net>
2016-11-03 20:31:34 +0100

    38: ECE 2016 Demos
    Task-Url: https://github.com/unitsofmeasurement/uom-demos/issues/issues/38

--- units-java8/src/main/java/si/uom/SI.java
+++ units-java8/src/main/java/si/uom/SI.java
@@ -113,7 +113,7 @@ public final class SI extends Units {
     /**
      * The SI unit for magnetomotive force (standard name <code>At</code>).
      */
-    public static final AlternateUnit<MagnetomotiveForce> AMPERE_TURN = addUnit(
+    public static final Unit<MagnetomotiveForce> AMPERE_TURN = addUnit(
 	    new AlternateUnit<MagnetomotiveForce>(SI.AMPERE, "At"), MagnetomotiveForce.class);
 
     //////////////////////////////
