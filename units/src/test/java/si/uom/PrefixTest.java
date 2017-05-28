300a5f873582747f82411bb98e88e7966d5410eb
Werner Keil <werner.keil@gmx.net>
2017-05-28 18:05:05 +0200

    30: Fix Codacy findings
    Task-Url: https://github.com/unitsofmeasurement/si-units/issues/issues/30

--- units/src/test/java/si/uom/PrefixTest.java
+++ units/src/test/java/si/uom/PrefixTest.java
@@ -53,8 +53,7 @@ public class PrefixTest {
     @Test
     public void testMega() {
 	Unit<Mass> m1 = MEGA(GRAM);
-	assertEquals("t", m1.toString()); // Passes on CI
-//	assertEquals("Mg", m1.toString());
+	assertEquals("t", m1.toString()); // Passes in Maeven/CI
     }
 
     @Test
