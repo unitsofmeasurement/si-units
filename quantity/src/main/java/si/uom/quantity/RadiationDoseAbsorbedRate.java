0d9dddc3e6862a8878f3feb4cc1f29caddb292c5
Werner Keil <werner.keil@gmx.net>
2024-01-16 15:09:54 +0100

    License header

--- quantity/src/main/java/si/uom/quantity/RadiationDoseAbsorbedRate.java
+++ quantity/src/main/java/si/uom/quantity/RadiationDoseAbsorbedRate.java
@@ -1,6 +1,6 @@
 /*
- * Units of Measurement API
- * Copyright (c) 2014-2021, Jean-Marie Dautelle, Werner Keil, Otavio Santana.
+ * International System of Units (SI)
+ * Copyright (c) 2005-2024, Jean-Marie Dautelle, Werner Keil and others.
  *
  * All rights reserved.
  *
@@ -13,8 +13,8 @@
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
  *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
- * 3. Neither the name of JSR-385 nor the names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * 3. Neither the name of SI System, Units of Measurement nor the names of their contributors may be used to
+ *    endorse or promote products derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
